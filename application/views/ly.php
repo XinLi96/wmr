9e0652dd8f0320c824e6d12381c1f297d88f9d38
XinLi96 <291750956@qq.com>
2017-03-27 11:16:14 +0800

    再次提交

---
+++ application/views/ly.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>留言板</title>
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet" href="assets/css/ly.css">
+</head>
+<body>
+    <?php include('header.php');?>
+    <div class="container wrapper">
+        <h1 class="title">留言板</h1>
+        <hr>
+        <ul>
+            <li class="cu">
+                <div class="cube">
+                    <span class="name"><a href="">栗鑫</a></span>
+                    <span class="time">2017-3-3 16:47</span>
+                    <div class="fgx"></div>
+                    <span class="content"><p class="sj">daslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasddaslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasddaslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasd</p></span>
+                </div>
+            </li>
+            <li class="cu">
+                <div class="cube">
+                    <span class="name"><a href="">栗鑫</a></span>
+                    <span class="time">2017-3-3 16:47</span>
+                    <div class="fgx"></div>
+                    <span class="content"><p class="sj">daslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasd</p></span>
+                </div>
+            </li>
+            <li class="cu">
+                <div class="cube">
+                    <span class="name"><a href="">栗鑫</a></span>
+                    <span class="time">2017-3-3 16:47</span>
+                    <div class="fgx"></div>
+                    <span class="content"><p class="sj">daslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasd</p></span>
+                </div>
+            </li>
+            <li class="cu">
+                <div class="cube">
+                    <span class="name"><a href="">栗鑫</a></span>
+                    <span class="time">2017-3-3 16:47</span>
+                    <div class="fgx"></div>
+                    <span class="content"><p class="sj">daslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasd</p></span>
+                </div>
+            </li>
+            <li class="cu">
+                <div class="cube">
+                    <span class="name"><a href="">栗鑫</a></span>
+                    <span class="time">2017-3-3 16:47</span>
+                    <div class="fgx"></div>
+                    <span class="content"><p class="sj">daslfaslfnasfjnajkgbjkasbfjkasbjkfaskjfnasd</p></span>
+                </div>
+            </li>
+        </ul>
+        <div class="fy">分页选项</div>
+    </div>
+    <?php include('footer.php');?>
+</body>
+</html>
